b9d11cee9f78d83d5a8b63f7a012c51a98f7838a
Regina Campos <rmscampos@gmail.com>
2020-01-23 11:47:03 -0600

    adds photo and link to github on nav bar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import CreateTodo from "./components/create-todo.component";
 import EditTodo from "./components/edit-todo.component";
 import TodosList from "./components/todos-list.component";
 
-import logo from "./logo.png";
+// import logo from "./logo.png";
 
 class App extends Component {
   render() {
@@ -15,14 +15,14 @@ class App extends Component {
         <div className="container">
           
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
-              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
-            </a>
-            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            {/* <a className="navbar-brand" href="https://github.com/rmscampos" target="_blank"> */}
+              {/* <img src={logo} width="30" height="30" alt="Regina" /> */}
+            {/* </a> */}
+            <Link to="/" className="navbar-brand">Regina's To Do App</Link>
             <div className="collpase nav-collapse">
               <ul className="navbar-nav mr-auto">
                 <li className="navbar-item">
-                  <Link to="/" className="nav-link">Todos</Link>
+                  <Link to="/" className="nav-link">To Do List</Link>
                 </li>
                 <li className="navbar-item">
                   <Link to="/create" className="nav-link">Create Todo</Link>
